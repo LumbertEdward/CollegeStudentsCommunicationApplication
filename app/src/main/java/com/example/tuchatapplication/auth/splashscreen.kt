204f7691e8bb7ed63d1c65521148e502d7e3b590
LumbertEdward <thiagolumbert@gmail.com>
2021-11-16 11:23:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tuchatapplication/auth/splashscreen.kt
@@ -0,0 +1,12 @@
+package com.example.tuchatapplication.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tuchatapplication.R
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
